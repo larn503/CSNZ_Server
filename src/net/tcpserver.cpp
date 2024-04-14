896c166b2f10a97fae77b901c9ff6ace45f0243a
JusicP <slender87844@gmail.com>
2024-04-14 12:59:21 +0300

    Don't change FD_SETSIZE define value to avoid memory corruption and crash in random places

--- src/net/tcpserver.cpp
+++ src/net/tcpserver.cpp
@@ -26,7 +26,7 @@ CTCPServer::CTCPServer() : m_ListenThread(ListenThread, this)
 
 #ifdef WIN32
 	WSADATA wsaData;
-	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 	if (result != 0)
 	{
 		Console().FatalError("WSAStartup() failed with error: %d\n%s\n", m_nResult, WSAGetLastErrorString());
@@ -197,10 +197,7 @@ void CTCPServer::Listen()
 			{
 				IExtendedSocket* socket = Accept(m_nNextClientIndex);
 				if (!socket)
-				{
-					SleepMS(100);
 					break;
-				}
 
 				Console().Log("Client (%d, %s) has been connected to the server\n", m_nNextClientIndex, socket->GetIP().c_str());
 
